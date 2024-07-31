3a65eb4c0edc7727b1f0478a98cace01150786bd
potabattulahemavardhani <148325977+potabattulahemavardhani@users.noreply.github.com>
2024-07-31 20:51:46 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Add
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a value");
+        a=sc.nextInt();
+        System.out.println("enter b value");
+        b=sc.nextInt();
+        a=2;
+        b=3;
+        c=a+b;
+        System.out.println("addition of a and b = "+c);
+    }
+}
